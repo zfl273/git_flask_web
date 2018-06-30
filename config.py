7acd087875a08a0a938e8180af3c6c6bf872a006
feifei <feifei@163.com>
2018-06-30 13:47:14 +0800

    项目立项&集成数据库&迁移

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 class Config(object):
     DEBUG = None
-
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:woaini2008@localhost:3306/git_flask_web'
+    # 动态追踪修改设置，如未设置只会提示警告
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class developmentConfig(Config):
     DEBUG = True
--- manage.py
+++ manage.py
@@ -1,10 +1,21 @@
 from flask import Flask, session
 from flask_script import Manager
 from config import config
-
+# 集成数据库扩展
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import MigrateCommand,Migrate
 app = Flask(__name__)
-manage = Manager(app)
 app.config.from_object(config['development'])
+
+db = SQLAlchemy(app)
+
+
+
+manage = Manager(app)
+# 使用迁移框架，绑定程序，数据库和命令
+Migrate(app, db)
+manage.add_command('db', MigrateCommand)
+
 @app.route('/')
 def index():
     return 'index'

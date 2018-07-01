3feb410476b4ae80ceed6ffde32bec8a40ba3f17
feifei <feifei@163.com>
2018-07-01 11:04:23 +0800

    项目立项&首页登录注册切换展示功能实现

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,16 +1,27 @@
 #导入蓝图对象
 from . import news_blue
-from flask import session, render_template, current_app
-
+from flask import session, render_template, current_app, jsonify
+from info.models import User
+from info.utils.response_code import RET
 
 
 
 
 @news_blue.route('/')
 def index():
+    user_id = session.get('user_id')# session为redis数据库
+    data = None
+    if user_id:
+        try:
+            user = User.query.get(user_id)
+        except Exception as e:
+            current_app.logger.error(e)
+            return jsonify(errno=RET.DBERR, errmsg='查询数据库失败')
+        data = {
+            'user_info': user.to_dict() if user else None
+        }
 
-
-    return render_template('news/index.html')
+    return render_template('news/index.html', data=data)
 
 # favicon
 @news_blue.route('/favicon.ico')
